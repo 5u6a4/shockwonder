45b9b9b69e359da555a040e37e9d745e4e3ee5fa
suba <subahama256@gmail.com>
2023-09-11 16:24:15 +0900

    player.csにspace keyが押されたときの処理を追加

--- Scripts/player.cs
+++ Scripts/player.cs
@@ -7,15 +7,23 @@ public class player : MonoBehaviour
     float ShockwonderLength;
     float speed;
 
+    public GameObject shokuwan;
+
     // Start is called before the first frame update
     void Start()
     {
         
+
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKey(KeyCode.Space))
+        {
+            Vector3 nowPos = this.transform.position;
+            Instantiate(shokuwan, nowPos,Quaternion.identity);
+        }
     }
 }
