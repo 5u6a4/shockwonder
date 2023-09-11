0e8b67f458e82b18c847356374c0684f24bc9ce0
suba <subahama256@gmail.com>
2023-09-11 14:33:43 +0900

    変数追加

--- Scripts/player.cs
+++ Scripts/player.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class player : MonoBehaviour
 {
+    float ShockwonderLength;
+    float speed;
+
     // Start is called before the first frame update
     void Start()
     {
