5eff497c11cfaa44e1759ab8290da234aea0878c
suba <subahama256@gmail.com>
2023-09-12 13:59:31 +0900

    add handling when spacekey pushed

--- Scripts/player.cs
+++ Scripts/player.cs
@@ -6,6 +6,8 @@ public class player : MonoBehaviour
 {
     float ShockwonderLength;
     float speed;
+    private bool shokuwan_flag;
+    public GameObject shokuwan;
     //nakamuraAtsushi come now !!
     // Start is called before the first frame update
     void Start()
@@ -16,6 +18,15 @@ public class player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKey(KeyCode.Space))
+        {
+            if (shokuwan_flag)
+            {
+                Vector3 nowPos = this.transform.position;
+                Instantiate(shokuwan, nowPos, Quaternion.identity);
+                shokuwan_flag = false;
+            }
+        }
+
     }
 }
