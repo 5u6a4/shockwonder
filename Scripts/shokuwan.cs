db2a5cdfa9c9bc60b0b7c2adfa49195f06170454
atsushi-abuku <atsushijump623@gmail.com>
2023-09-13 15:51:01 +0900

    修正案by仲村敦

--- Scripts/shokuwan.cs
+++ Scripts/shokuwan.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class shokuwan : MonoBehaviour
 {
+    public float length;
     private Rigidbody rb;
     public float velocity = 5f;
 
@@ -11,13 +12,15 @@ public class shokuwan : MonoBehaviour
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        rb.velocity = new Vector3(5.0f, 2.0f, 0.0f);
 
     }
 
     // Update is called once per frame
     void Update()
     {
-        rb.velocity = new Vector3(5.0f, 2.0f, 0.0f);
+
+        //rb.velocity = new Vector3(5.0f, 2.0f, 0.0f);
 
         
     }
